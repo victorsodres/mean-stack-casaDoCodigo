839d19f40e095c4ac972036cc0cfb33a72872cfe
Victor Sodré <victor.santos@metasix.com.br>
2015-02-05 22:16:42 -0200

    Segundo capitulo completo ~

--- contatooh/config/express.js
+++ contatooh/config/express.js
@@ -1,5 +1,7 @@
 //config/express.js
 var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function(){
     var app = express();
@@ -12,6 +14,15 @@ module.exports = function(){
 
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+
+    load('models', {cwd: 'app'})
+        .then('controllers')
+        .then('routes')
+        .into(app);
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
 
     return app;
 }
