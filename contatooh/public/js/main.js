2cb3a2f06c466dbf797cf273cb693bb1fb6e6143
Victor Sodré <victor.santos@metasix.com.br>
2015-02-19 20:47:08 -0200

    Capitulo 4

--- contatooh/public/js/main.js
+++ contatooh/public/js/main.js
@@ -1,1 +1,15 @@
-angular.module('contatooh', []);
+angular.module('contatooh', ['ngRoute'])
+ .config(function($routeProvider) {
+
+    $routeProvider.when('/contatos', {
+        templateUrl: 'partials/contatos.html',
+        controller: 'ContatosController'
+    });
+
+    $routeProvider.when('/contatos/:contatoId', {
+        templateUrl: 'partials/contato.html',
+        controller: 'ContatoController'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/contatos'});
+});
