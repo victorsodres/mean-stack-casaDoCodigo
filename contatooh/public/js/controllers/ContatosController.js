f688172b42d5e70f253d185651c0d6c494fc82a8
Victor Sodré <victor.santos@metasix.com.br>
2015-02-20 00:13:05 -0200

    Começando a utilzar o modulo $resource no angularjs

--- contatooh/public/js/controllers/ContatosController.js
+++ contatooh/public/js/controllers/ContatosController.js
@@ -1,27 +1,16 @@
 angular.module('contatooh')
-.controller('ContatosController', function($scope) {
+.controller('ContatosController', function($scope, $resource) {
     $scope.total = 0;
-    $scope.incrementa = function(){
-        $scope.total++;
-    };
-
-    $scope.contatos = [
-        {
-        "_id":  1,
-        "nome": "Contato Angular 1",
-        "email":"cont1@empresa.com.br"
-        },
-        {
-        "_id":  2,
-        "nome": "Contato Angular 2",
-        "email":"cont2@empresa.com.br"
-        },
-        {
-        "_id":  3,
-        "nome": "Contato Angular 3",
-        "email":"cont3@empresa.com.br"
-        }
-    ];
-
+    $scope.contatos = [];
     $scope.filtro = '';
+    
+    $http.get('/contatos')
+        .success(function(data){
+            $scope.contatos = data;
+        })
+        .error(function(error){
+            console.log('Não foi possível obter a lista de contatos.');
+            console.log(error.statusText);
+        })
+    
 });
